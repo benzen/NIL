a40ca41c40ae79c3db330e564001c19aa3036f12
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-11-23 13:54:26 -0500

    add grunt task

---
+++ tasks/nil.js
@@ -0,0 +1,27 @@
+/*
+ * grunt-nil
+ *
+ * Copyright (c) 2014 Benjamin Dreux
+ * Licensed under the MIT license.
+ * https://github.com/benzen/NIL/blob/master/LICENSE-MIT
+ */
+nil = require("../lib/nil");
+_ = require("lodash");
+module.exports = function(grunt) {
+  grunt.registerTask('nil', "migrate data base",function(operation, upDownScript) {
+    var done = this.async();
+    if(!this.options().connectionString){
+      grunt.log.error("Connection String is required");
+      done(false);
+      return;
+    };
+    var args = [this.options().connectionString, this.options().migrationFolder, upDownScript, function(err, de){ console.log(err, de); done(err) } ];
+    if(!_.contains(["create", "init", "up", "down"], operation)){
+      var msg = "Unsupported operation"
+      grunt.log.error(msg);
+      done(false);
+      return
+    }
+    nil[operation].apply(this, args);
+  });
+};
